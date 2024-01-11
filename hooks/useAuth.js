52b808c3bbb90b1e603ed579909a0cde33c846e1
Nah-Nova <noaheutz@hotmail.nl>
2024-01-11 15:22:17 +0100

    feat: adds auth hook and env

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,9 +1,13 @@
 import { View, Text } from 'react-native'
 import React, { createContext, useContext } from 'react'
+import * as Google from 'expo-auth-session';
 
 const AuthContext = createContext({})
 
 export const AuthProvider = ({ children }) => {
+	const signInWithGoogleAsync = async () => {
+		await Google.loadAsync()
+	}		
   return (
 	<AuthContext.Provider 
 		value={{
