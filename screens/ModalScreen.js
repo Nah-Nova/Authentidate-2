9807b68a74774e2e0f11d8230d2f39ec438ed4f9
Nah-Nova <noaheutz@hotmail.nl>
2024-01-14 15:40:11 +0100

    feat: gets profile’s values on modal

--- screens/ModalScreen.js
+++ screens/ModalScreen.js
@@ -6,13 +6,44 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
-import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
 import { useNavigation } from "@react-navigation/native";
 import * as Location from "expo-location";
 import MapView from "react-native-maps";
 import useAuth from "../hooks/useAuth";
 import { db } from "../firebase";
+
+// array of interests with icon type
+const interestOptions = [
+  "Photography",
+  "Shopping",
+  "Karaoke",
+  "Travel",
+  "Fitness",
+  "Reading",
+  "Coding",
+  "Cooking",
+  "Gaming",
+  "Art",
+  "Movies",
+  "Technology",
+  "Writing",
+  "Coffee",
+  "Soccer",
+  "Pets",
+  "Yoga",
+  "Crafting",
+  "Science",
+  "Dancing",
+];
 
 const ModalScreen = () => {
   const { userInfo } = useAuth();
@@ -20,9 +51,28 @@ const ModalScreen = () => {
   const [picture, setPicture] = useState(null);
   const [job, setJob] = useState(null);
   const [age, setAge] = useState(null);
+  const [interests, setInterests] = useState([]);
   const [location, setLocation] = useState(null);
 
-  const incompleteProfile = !picture || !job || !age;
+  const [selectedInterests, setSelectedInterests] = useState([]);
+
+  const handleInterestToggle = (interest) => {
+    // Check if the interest is already selected
+    const isInterestSelected = selectedInterests.includes(interest);
+
+    // Update the selected interests array accordingly
+    if (isInterestSelected) {
+      setSelectedInterests((prevInterests) =>
+        prevInterests.filter(
+          (selectedInterest) => selectedInterest !== interest
+        )
+      );
+    } else {
+      setSelectedInterests((prevInterests) => [...prevInterests, interest]);
+    }
+  };
+
+  const incompleteProfile = !picture || !job || !age || !interests;
 
   const updateUserProfile = () => {
     setDoc(doc(db, "users", userInfo.uid), {
@@ -35,6 +85,7 @@ const ModalScreen = () => {
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
       },
+      interests: selectedInterests,
       timestamp: serverTimestamp(),
     })
       .then(() => {
@@ -46,6 +97,27 @@ const ModalScreen = () => {
       });
   };
 
+  const getUserProfile = async () => {
+    const docRef = doc(db, "users", userInfo.uid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      const { photoURL, job, age, interests } = docSnap.data();
+      setPicture(photoURL);
+      setJob(job);
+      setAge(age);
+      setInterests(interests);
+      setSelectedInterests(interests);
+    } else {
+      console.log("No such document!");
+    }
+  };
+
+  useEffect(() => {
+    getUserProfile();
+  }, []);
+
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -56,7 +128,6 @@ const ModalScreen = () => {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
-      console.log(location);
     })();
   }, []);
 
@@ -68,22 +139,25 @@ const ModalScreen = () => {
         resizeMode="contain"
       />
       <Text style={styles.modalText}>Welcome {userInfo?.displayName}</Text>
-      <View style={styles.formContainer}>
-        <Text style={styles.modalTitle}>1. Add profile picture URL</Text>
+      <ScrollView
+        style={styles.formContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        <Text style={styles.modalTitle}>1. Profile photo url</Text>
         <TextInput
           style={styles.modalInput}
           placeholder="Enter URL here"
           value={picture}
           onChangeText={setPicture}
         />
-        <Text style={styles.modalTitle}>2. Add job title</Text>
+        <Text style={styles.modalTitle}>2. Occupation</Text>
         <TextInput
           style={styles.modalInput}
           placeholder="Enter current occupation"
           value={job}
           onChangeText={setJob}
         />
-        <Text style={styles.modalTitle}>3. Add age</Text>
+        <Text style={styles.modalTitle}>3. Age</Text>
         <TextInput
           style={styles.modalInput}
           placeholder="Enter current age"
@@ -92,20 +166,44 @@ const ModalScreen = () => {
           keyboardType="numeric"
           maxLength={2}
         />
-        <Text style={styles.modalTitle}>4. Location</Text>
+        <Text style={styles.modalTitle}>4. Interests</Text>
+        {/* TODO:   create a list with 2 vertical colums with buttons to select interestOptions and add them to an array of interests */}
+        <View style={styles.interestsContainer}>
+          {interestOptions.map((interest, index) => (
+            <TouchableOpacity
+              key={index}
+              style={[
+                styles.interestButton,
+                selectedInterests.includes(interest) && styles.selectedInterest,
+              ]}
+              onPress={() => handleInterestToggle(interest)}
+            >
+              <Text
+                style={[
+                  styles.interestButtonText,
+                  selectedInterests.includes(interest) &&
+                    styles.selectedInterestText,
+                ]}
+              >
+                {interest}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <Text style={styles.modalTitle}>5. Location</Text>
         {location && (
-        <MapView
-          style={{ width: "100%", height: 200, borderRadius: 16 }}
-          showsUserLocation 
-          initialRegion={{
-            latitude: location?.coords.latitude,
-            longitude: location?.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
+          <MapView
+            style={{ width: "100%", height: 200, borderRadius: 16 }}
+            showsUserLocation
+            initialRegion={{
+              latitude: location?.coords.latitude,
+              longitude: location?.coords.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
         )}
-      </View>
+      </ScrollView>
       <TouchableOpacity
         style={[styles.modalButton, { opacity: incompleteProfile ? 0.3 : 1 }]}
         onPress={updateUserProfile}
@@ -167,5 +265,33 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold",
     color: "#ffffff",
+  },
+  interestsContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    marginBottom: 16,
+  },
+  interestButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    borderWidth: 1,
+    borderColor: "#D8D8D8",
+    borderRadius: 16,
+    padding: 16,
+    marginVertical: 8,
+    width: "48%",
+  },
+  selectedInterest: {
+    backgroundColor: "#CC2248", // Change the background color for selected interests
+  },
+  selectedInterestText: {
+    color: "#ffffff", // Change the text color for selected interests
+  },
+  interestButtonText: {
+    marginLeft: 8,
+    fontSize: 14,
+    color: "#000",
   },
 });
